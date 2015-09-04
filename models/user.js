81655cc35dc069dc122c25178a3cc182b784e541
benhunt29 <benhunt29@gmail.com>
2015-09-04 16:37:47 -0500

    added first name, last name, and email fields

--- models/user.js
+++ models/user.js
@@ -5,7 +5,10 @@ var mongoose = require('mongoose'),
 
 var UserSchema = new Schema({
     username: { type: String, required: true, index: {unique:true}},
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    firstName: String,
+    lastName: String,
+    email: { type: String, required: true }
 });
 
 UserSchema.pre('save',function(next){
