1885be19a630a749c7d66fdae43efa2edfa866b5
benhunt29 <benhunt29@gmail.com>
2015-09-05 14:49:24 -0500

    added jquery and app.js to front-end

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use(express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
 app.use(session({
   secret: 'secret',
   key: 'user',
